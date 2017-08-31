a1a9e5ee0e45af662f5640a2eb99ff5e6bf1c348
Unknown <A01363997@itesm.mx>
2017-08-31 07:01:00 -0500

    Multipler

---
+++ Booth Multiplier/boothmult.v
@@ -0,0 +1,7 @@
+module boothmult(
+	input[5:0] A,
+	input[5:0] B,
+	output[11:0] R
+);
+
+endmodule
---
+++ Booth Multiplier/fsmboothmult.v
@@ -0,0 +1,12 @@
+module fsmboothmult(
+	input start,
+	input valid,
+	output prodload,
+	output multload,
+	output 
+);
+
+
+
+
+endmodule
